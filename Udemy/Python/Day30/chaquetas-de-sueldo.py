3159d87558da572f5c0e1bd5657969837cd6ed2d
DanielBorbolla <danielborbollasuarez88@gmail.com>
2023-05-29 11:48:57 -0600

    update security key

--- Udemy/Python/Day30/chaquetas-de-sueldo.py
+++ Udemy/Python/Day30/chaquetas-de-sueldo.py
@@ -3,11 +3,11 @@ import random
 inflacion_anual = (3.5)/100
 
 ano_inicio = 2006
-ano_final = 2023
+ano_final = 2033
 sueldo_inicial = 24000
 sueldo_actualizado = sueldo_inicial
 for i in range(ano_inicio, ano_final):
-    variacion = (random.uniform(2, 7))/100
+    variacion = (random.uniform(5, 10))/100
     # también podemos usar random.random() pero este solo genera números entre 0 y 1
     inflacion_total = (inflacion_anual+variacion)
     sueldo_actualizado *= (1+inflacion_total)
